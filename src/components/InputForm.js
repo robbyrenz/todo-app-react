73b315020043763aaac309977e7b8a49cd4e99e6
Robby Renz <nxgrenz@gmail.com>
2019-10-30 22:40:21 -0500

    remove unnecessary files, add starter files and components

---
+++ src/components/InputForm.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import ListItem from "./ListItem"
+
+class InputForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            item: "",
+            items: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="item"
+                        value={this.state.item}
+                        placeholder="Enter your todo here"
+                        onChange={this.handleChange}
+                    />
+                    <button>Add Item</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default InputForm
---
+++ src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ListItem(props) {
+    return (
+        <div>
+            <li>{props.item}</li>
+        </div>
+    )
+}
+
+export default ListItem
